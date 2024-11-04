d3d773fcc10f7c7a488254055ae4c5f537e4cfef
ashenone1379 <2920130195@qq.com>
2024-11-05 02:04:14 +0800

    完成PART A2

--- steganography.c
+++ steganography.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **				Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				HQY
 **
 ** DATE:        2020-08-23
 **
@@ -18,16 +18,37 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
-//Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
-Color *evaluateOnePixel(Image *image, int row, int col)
-{
-	//YOUR CODE HERE
+//Determines what color the cell at the given row/col should be.
+//This should not affect Image, and should allocate space for a new Color.
+Color *evaluateOnePixel(Image *image, int row, int col) {
+    Color *result = malloc(sizeof(Color));
+    result->R = image->image[row][col].R;
+    result->G = image->image[row][col].G;
+    result->B = image->image[row][col].B;
+
+    return result;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
-Image *steganography(Image *image)
-{
-	//YOUR CODE HERE
+Image *steganography(Image *image) {
+    const int rows = image->rows, cols = image->cols;
+    Image *new_image = malloc(sizeof(Image));
+    new_image->rows = rows;
+    new_image->cols = cols;
+
+    new_image->image = malloc(sizeof(Color *) * rows);
+    for (int i = 0; i < rows; ++i) {
+        new_image->image[i] = malloc(sizeof(Color) * cols);
+        for (int j = 0; j < cols; ++j) {
+            Color *color = evaluateOnePixel(image, i, j);
+            new_image->image[i][j].R = color->R;
+            new_image->image[i][j].G = color->G;
+            new_image->image[i][j].B = (color->B & 1) ? 0 : 255;
+            free(color);
+        }
+    }
+
+    return new_image;
 }
 
 /*
@@ -43,7 +64,17 @@ If the input is not correct, a malloc fails, or any other error occurs, you shou
 Otherwise, you should return from main with code 0.
 Make sure to free all memory before returning!
 */
-int main(int argc, char **argv)
-{
-	//YOUR CODE HERE
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        return -1;
+    }
+
+    Image *img = readData(argv[1]);
+
+    img = steganography(img);
+
+    writeData(img);
+
+    free(img);
+    return 0;
 }
